80951de4bef125bb287ef1f9f42122620027ea3a
Harry Dong <hhdong@uwaterloo.ca>
2018-01-18 16:52:25 -0500

    cleaned up

--- index.php
+++ index.php
@@ -6,22 +6,19 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="This is not a porn site">
+    <meta name="description" content="Harry's Website">
     <meta name="author" content="Harry Dong">
 
-    <title>Harry Dong</title>
+    <title>Harry's website</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="css/animate.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
     <link href="css/auto.css" rel="stylesheet">
     <link href="css/auto-nav.css" rel="stylesheet">
 
-
     <script src="js/jquery.min.js"></script>
-    <script src="js/jquery.color.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/easing.js"></script>
     <script src="js/wow.min.js"></script>
@@ -33,37 +30,11 @@
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-    <!-- Navigation -->
-    <nav id="navbar" class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button id="btn-nav-toggle" onclick="toggleNav()" type="button" class="navbar-toggle">
-                    <span class="icon-bar top-bar"></span>
-                    <span class="icon-bar middle-bar"></span>
-                    <span class="icon-bar bottom-bar"></span>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">harrydong.com</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden"><a href="#page-top"></a></li>
-                    <li><a class="page-scroll" href="#">harry</a></li>
-                    <li><a class="page-scroll" href="#">dong</a></li>
-                    <!--<li><a href="#"><span id="cart-txt">Cart</span><i id="cart-i" class="fa fa-shopping-cart fa-2x"></i><span id="cart-quantity">1</span></a></li>-->
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
     <!-- Intro Header -->
     <header class="intro">
         <div class="col-sm-12">
-            <h1>This is not a porn site</h1>
+            <h1>Harry's in progress website</h1>
         </div>
         <!--<a id="continue-btn" href="#about" class="btn btn-circle page-scroll">
             <i class="fa fa-angle-down fa-4x"></i>
--- teadaddy.php
+++
@@ -1,6 +0,0 @@
-<?php
-$info = json_decode(file_get_contents("teadaddy.json"),true);
-$info['currentTime'] = date("H:i:s");
-echo json_encode($info);
-
- ?>
