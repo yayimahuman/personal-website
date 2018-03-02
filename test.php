3639f3be3a6c56c68721ddb095ece14ca3a6115b
Harry Dong <hhdong@uwaterloo.ca>
2018-03-01 22:14:21 -0500

    updated bootstrap and fixed broken stuff. worked on cards

--- test.php
+++ test.php
@@ -14,16 +14,16 @@
     <link rel="shortcut icon" href="img/harry.png" type="image/x-icon" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="css/animate.css" rel="stylesheet" type="text/css">
-    <link  href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Mukta+Malar:300" rel="stylesheet">
+    <!-- <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"> -->
 
     <link href="css/harry-test.css" rel="stylesheet">
 
     <script src="js/jquery.min.js"></script>
     <!-- <script src="js/bootstrap.min.js"></script> -->
-    <script src="js/easing.js"></script>
+    <!-- <script src="js/easing.js"></script>
     <script src="js/wow.min.js"></script>
-    <script type="text/javascript" src="js/smooth-parallax.min.js"></script>
+    <script type="text/javascript" src="js/smooth-parallax.min.js"></script> -->
 
     <script src="js/harry-test.js"></script>
 
@@ -55,20 +55,263 @@
 
 
 
-    <section id="about">
+    <!-- <section id="about">
 
 
 
-    </section>
+    </section> -->
+
+
+    <section id="projects" class="content-section">
 
 
 
 
-    <section id="projects">
+        <div class="deck">
+
+            <!-- wide card -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-static">
+
+                    <div class="card-img">
+                        <img class="img-responsive" src="https://i1.wp.com/www.kenlyonsphotography.com.au/wp-content/uploads/2014/10/140409-London-England-195928-1024px.jpg?ssl=1"/>
+
+                    </div>
+
+                    <div class="card-txt">
+                        <h2 class="card-txt-title">Project Title</h2>
+                    </div>
+
+                    <div class="card-footer">
+                        <ul class="card-footer-wrapper">
+                            <li><code class="js">JS</code></li>
+                            <li><code class="php">&lt;?php&gt;</code></li>
+                            <li><code class="mysql">MySQL<span style="color:#ededed">*</span></code></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- wide card -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-static">
+
+                    <div class="card-img">
+                        <img class="img-responsive" src="http://static.flickr.com/43/102997171_f9263d8797_o.jpg"/>
+
+                    </div>
+
+                    <div class="card-txt">
+                        <h2 class="card-txt-title">Project Title</h2>
+                    </div>
+
+                    <div class="card-footer">
+                        <ul class="card-footer-wrapper">
+                            <li><code class="html">&lt;<span style="color:#f75d50">html</span>&gt;</code></li>
+                            <li><code class="css">.CSS</code></li>
+                            <li><code class="js">JS</code></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- wide card -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-static">
+
+                    <div class="card-img">
+                        <img class="img-responsive" src="https://ifsstech.files.wordpress.com/2008/10/169.jpg"/>
+
+                    </div>
+
+                    <div class="card-txt">
+                        <h2 class="card-txt-title">Project Title</h2>
+                    </div>
+
+                    <div class="card-footer">
+                        <ul class="card-footer-wrapper">
+                            <li><code class="html">&lt;<span style="color:#f75d50">html</span>&gt;</code></li>
+                            <li><code class="css">.CSS</code></li>
+                            <li><code class="js">JS</code></li>
+                            <li><code class="php">&lt;?php&gt;</code></li>
+                            <li><code class="mysql">MySQL<span style="color:#ededed">*</span></code></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="deck">
+
+            <!-- wide card with shuttered description -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-shutter active">
+                    <div class="card-img">
+
+                    </div>
+                    <div class="card-txt">
+
+                        <h2 class="card-txt-title">Title</h2>
+
+                        <p class="card-txt-desc">desc</li>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- wide card with shuttered description -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-shutter active">
+                    <div class="card-img">
+
+                    </div>
+                    <div class="card-txt">
+
+                        <h2 class="card-txt-title">Title</h2>
+
+                        <p class="card-txt-desc">desc</li>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- wide card with shuttered description -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-shutter active">
+                    <div class="card-img">
+
+                    </div>
+                    <div class="card-txt">
+
+                        <h2 class="card-txt-title">Title</h2>
+
+                        <p class="card-txt-desc">desc</li>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="deck">
+
+
+            <!-- wide card with description on backside -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-flip">
+                    <div class="card-img">
+
+                    </div>
+                    <div class="card-txt">
+
+                        <h2 class="card-txt-title">Title</h2>
+
+                        <!-- <p class="card-txt-desc">desc</li> -->
+
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- wide card with description on backside -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-flip">
+                    <div class="card-img">
+
+                    </div>
+                    <div class="card-txt">
+
+                        <h2 class="card-txt-title">Title</h2>
+
+                        <!-- <p class="card-txt-desc">desc</li> -->
+
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- wide card with description on backside -->
+            <div class="card-wrapper col-sm-4">
+                <div class="card card-wide card-flip">
+                    <div class="card-img">
+
+                    </div>
+                    <div class="card-txt">
+
+                        <h2 class="card-txt-title">Title</h2>
+
+                        <!-- <p class="card-txt-desc">desc</li> -->
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
 
 
 
-        
+        <div class="clearfix"></div>
+
+
+
+        <div class="deck">
+            <!-- tall card with info already displayed -->
+            <div class="card card-tall card-static">
+                <div class="card-img">
+
+                </div>
+                <div class="card-txt">
+                    <h2 class="card-txt-title"></h2>
+
+                    <p class="card-txt-desc"></li>
+
+                </div>
+
+            </div>
+
+            <!-- tall card with shuttered description -->
+            <div class="card card-tall card-shutter">
+                <div class="card-img">
+
+                </div>
+                <div class="card-txt">
+                    <h2 class="card-txt-title"></h2>
+
+                    <p class="card-txt-desc"></li>
+
+                </div>
+
+            </div>
+
+            <!-- tall card with description on backside -->
+            <div class="card card-tall card-flip">
+                <div class="card-img">
+
+                </div>
+                <div class="card-txt">
+                    <h2 class="card-txt-title"></h2>
+
+                    <p class="card-txt-desc"></li>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
     </section>
 
 
