7a4a33d71d25f05c14a18bb8f5e63a33deea7f05
Harry Tong <harry.tong@uwaterloo.ca>
2019-09-24 22:25:49 -0700

    Update linkedin URL and firebase cache

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export const RESUME_LINK = "https://drive.google.com/file/d/1eGHnypBN9OWqlRCOpAzRYj51FtR2FFd_/view?usp=sharing";
+export const RESUME_LINK = "https://drive.google.com/file/d/1XqIGDOdTOSXN78o47G69T0mNutgOnq_w/view?usp=sharing";
 
 export const GITHUB_LINK = "https://github.com/yayimahuman";
 
-export const LINKEDIN_LINK = "https://www.linkedin.com/in/harry-dong/";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/yayimahuman/";
 
 export const EMAIL_LINK = "mailto:harry.tong@uwaterloo.ca";
