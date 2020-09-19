849f548315369a0e8cb3dab70a058f30082c8559
Harry Tong <harry.tong@uwaterloo.ca>
2020-09-19 00:38:30 -0400

    Update resume

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const RESUME_LINK = "https://drive.google.com/file/d/1-dNkh2zQhcAmvzCWO0mX7HHiaEi49rAI/view?usp=sharing";
+export const RESUME_LINK = "https://drive.google.com/file/d/1h11bEJQVKmABdgSkUvz87GN35vJ0Ehd9/view?usp=sharing";
 
 export const GITHUB_LINK = "https://github.com/yayimahuman";
 
