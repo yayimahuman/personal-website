ee921c7f98e53761da5fea7b4e2fc226e4296a38
Harry Tong <yayimahuman@gmail.com>
2025-07-02 21:21:06 -0400

    Add substack link

---
+++ src/components/SubstackIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {ReactComponent as SubstackLogo} from "./SubstackLogo.svg";
+
+const SubstackIcon = ({size = "2x", className = ""}) => {
+	const sizeClass = size === "2x" ? "fa-2x" : `fa-${size}`;
+	
+	return (
+		<SubstackLogo 
+			className={`svg-inline--fa fa-substack ${sizeClass} ${className}`}
+			role="img"
+			aria-hidden="true"
+			focusable="false"
+		/>
+	);
+};
+
+export default SubstackIcon; 
